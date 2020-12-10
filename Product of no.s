b1ef9e49927600b771750483623dc9fff0ef6563
shrutitiwari19 <75686347+shrutitiwari19@users.noreply.github.com>
2020-12-10 21:17:34 +0530

    Create Product of no.s

---
+++ Product of no.s
@@ -0,0 +1,15 @@
+ #include <iostream.h>
+ #include <conio.h>
+
+int main()
+{
+    double f, s, p;
+    cout << "Enter two numbers: ";
+    cin >> f >> s;
+ 
+    p = f * s;  
+
+    cout << "Product = " << p;    
+    
+    return 0;
+}
